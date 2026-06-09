a94504527d27c122ae90a12e47c87de595087333
Aniket Kumar Mahato <142729831+aniketxz@users.noreply.github.com>
2026-06-09 11:53:48 +0530

    Time: 9 ms (27.23%) | Memory: 19.3 MB (66.50%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,47 +1,46 @@
+// using kahn's algorithm
+// If we can produce a topological ordering containing all vertices, then there is no cycle
 class Solution {
 public:
-    bool canToposort(int node, vector<bool>& visited, 
-        vector<bool>& visitedPath, vector<vector<int>>& adjacencyList) {
-        
-        visited[node] = true;
-        visitedPath[node] = true;
-
-        for(int neighbour : adjacencyList[node]) {
-            if(!visited[neighbour]) {
-                if(!canToposort(neighbour, visited, visitedPath, adjacencyList))
-                    return false;
-            } else if(visitedPath[neighbour]) {
-                return false;
-            }
-        }
-
-        visitedPath[node] = false;
-        return true;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         const int V = numCourses;
 
         // build adjacency list
         vector<vector<int>> adjacencyList(V);
+        vector<int> indegree(V, 0);
+
         for(auto& it : prerequisites) {
             int u = it[1];
             int v = it[0];
+
             adjacencyList[u].push_back(v);
+            indegree[v]++;
         }
 
-        // using toposort to determine if we can linearly represent the dependencies
-        vector<bool> visited(V, false);
-        vector<bool> visitedPath(V, false);
-
-        // handling disconnected components
-        for(int node = 0; node < V; node++) {
-            if(!visited[node]) {
-                if(!canToposort(node, visited, visitedPath, adjacencyList))
-                    return false;
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        return true;
+        // keep track of processed instead of storing in a vector
+        int processed = 0;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            processed++;
+
+            for(int neighbour : adjacencyList[node]) {
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0) {
+                    q.push(neighbour);
+                }
+            }
+        }
+
+        return processed == V;
     }
 };
